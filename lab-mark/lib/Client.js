50cd65774d105a8d2af83c6c5e721ce94ec01063
Mackoyokcam <mark.hangarter@gmail.com>
2017-09-18 23:03:56 -0700

    Added client constructor and fixed small server errors

---
+++ lab-mark/lib/Client.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function Client(socket, nickname, remoteAddress) {
+  this.socket = socket;
+  this.nickname = nickname;
+  this.id = remoteAddress.split(':').slice(-1)[0]; // Gets IP of connecting client.
+};
--- lab-mark/lib/server.js
+++ lab-mark/lib/server.js
@@ -2,17 +2,33 @@
 
 const net = require('net');
 const faker = require('faker');
+const Client = require('./Client.js');
 
 const app = net.createServer();
-let clients = [];
+
+let validCommands = {
+  '@help': 'Lists all of these commands',
+  '@list': 'Lists all the users connected to the chatroom by nickname.',
+  '@quit': 'Allows users to leave the chatroom.',
+  '@nickname <new name>': 'Sets user\'s nickname to new name.',
+  '@dm <user> <message>': 'Sends a direct message to specified user.',
+};
+
+let clientPool = [];
 
 app.on('connection', (socket) => {
-  socket.nickname = faker.internet.userName();
-  clients.push(socket);
-  socket.write('Welcome to the chatroom!\n');
-  socket.write(`Your nickname is ${socket.nickname}\n`);
+  let thisClient = new Client(socket, faker.internet.userName(), socket.remoteAddress);
+  clientPool.push(thisClient);
 
-  // Even handlers
+  socket.write('Welcome to the chatroom!\n');
+  socket.write(`Your current nickname is ${thisClient.nickname}\n`);
+  socket.write(`Type "@help" for a list of chatroom commands.\n`);
+
+
+  // Notify chatroom of new user.
+  sayToAll(`${thisClient.nickname} has joined the chatroom.\n`);
+
+  // Socket event handlers
   socket.on('data', (data) => {
     let message = data.toString().trim();
 
@@ -23,66 +39,89 @@ app.on('connection', (socket) => {
       let value = words.slice(1).join(' ');
 
       switch(command){
+      case '@help': {
+        for (let key in validCommands) {
+          socket.write(`${key}: ${validCommands[key]}\n`);
+        }
+        return;
+      }
       case '@quit': {
-        socket.end(`Goodbye ${socket.nickname}!\n`);
+        socket.end(`Goodbye ${thisClient.nickname}!\n`);
+        // sayToAll(`${thisClient.nickname} left the chatroom.\n`);
         return;
       }
       case '@list': {
-        let nicknames = clients.map(c => c.nickname);
+        let nicknames = clientPool.map(client => client.nickname);
         socket.write(nicknames.join('\n') + '\n');
         return;
       }
       case '@nickname': {
-        let nicknames = clients.map(c => c.nickname);
+        let nicknames = clientPool.map(client => client.nickname);
 
+        // No new name entered.
         if (!value)
-          return socket.write('Enter a nickname after "@nickname.\n"');
+          return socket.write('Usage: @nickname <new name>\n"');
 
+        // Nickname already taken.
         if (nicknames.includes(value))
-          return socket.write(`The nickname ${value} is already taken.`);
+          return socket.write(`The nickname "${value}" is already taken.\n`);
 
-        socket.nickname = value;
+        let oldName = thisClient.nickname;
+        thisClient.nickname = value;
         socket.write(`Your new nickname is: ${value}\n`);
+
+        sayToAll(`${oldName} changed their name to ${thisClient.nickname}\n`);
         return;
       }
       case '@dm': {
-        let nicknames = clients.map(c => c.nickname);
+        let nicknames = clientPool.map(client => client.nickname);
 
-        let messagee = words[1];
+        let client = words[1];
         let message = words.slice(2).join(' ');
 
         // Check if message was entered
         if (!message)
-          return socket.write(`Enter a message after @dm <username>\n`);
+          return socket.write(`Usage: @dm <user> <message>\n`);
 
         // Check if user exists.
-        if(!nicknames.includes(messagee))
-          return socket.write(`The user ${value} is not connected to the server.\n`);
+        if(!nicknames.includes(client))
+          return socket.write(`The user "${client}" is not connected to the server.\n`);
 
-        let index = nicknames.indexOf(messagee);
-        clients[index].write(`DM from ${socket.nickname}: ${value}\n`);
+        let index = nicknames.indexOf(client);
+        clientPool[index].socket.write(`DM from ${thisClient.nickname}: ${message}\n`);
 
         return;
       }
       default: {
-        socket.write('try @list or @nickname\n');
+        socket.write('type @help for a list of valid commands.\n');
+        return;
       }
       }
     }
 
-    clients.forEach((client) => {
-      if(client !== socket)
-        client.write(`${socket.nickname}: ${message}\n`);
-    });
+    sayToAll(`${thisClient.nickname}: ${message}\n`);
   });
 
+  // Notifies all other users besides client triggering event.
+  function sayToAll(message) {
+    clientPool.forEach((client) => {
+      if(client.socket !== socket)
+        client.socket.write(message);
+    });
+  }
+
   let removeClient = (socket) => () => {
-    clients = clients.filter((client) => {
-      return client !== socket;
+    sayToAll(`${thisClient.nickname} left the chatroom.\n`);
+    clientPool = clientPool.filter((client) => {
+      return client.socket !== socket;
     });
   };
 
-  socket.on('error', removeClient(socket));
+  socket.on('error', (err) => {
+    console.error('__SOCKET_ERROR__', err);
+    removeClient(socket);
+  });
+
   socket.on('close', removeClient(socket));
 });
 
